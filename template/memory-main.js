4447dc7d8c129c3df09552258bce903f195697ee
AnaBenn <anastasia.n.bennett@gmail.com>
2015-09-02 15:28:29 -0700

    Update memory-main.js

--- template/memory-main.js
+++ template/memory-main.js
@@ -1,10 +1,16 @@
 
-var cards,game,gui; //global vars can been inspected in console, making debugging easier
+var cards, game, gui; //global vars can been inspected in console, making debugging easier
 function go() {
-	// you'll need to study the modules to understand how to plug them in...
-	cards = //??????
-	game  = //??????
-	gui   = new MemoryGUI('memorygame',game); //'memorygame' is the id of div where gui should be inserted'
+  // you'll need to study the modules to understand how to plug them in...
+  cards = new MemoryCards(),
+    game = new MemoryGame(cards),
+    gui = new MemoryGUI('memorygame', game); //'memorygame' is the id of div where gui should be inserted'
+
+  var resetButton = document.createElement('button');
+  resetButton.innerHTML = 'S T A R T / R E S T A R T';
+  resetButton.classList.add('resetButton');
+  resetButton.addEventListener('click', gui.reset);
+  document.getElementById('memorygame').appendChild(resetButton);
 }
 
-window.addEventListener("load",go);
+window.addEventListener("load", go);
